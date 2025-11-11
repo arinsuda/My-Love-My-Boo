d72b048d21c96f52ddbbec39da202f1e9db9d2ac
arinsuda <28irin62@gmail.com>
2025-11-11 11:29:40 +0700

    fix duplicate Handler by renaming to Health & Moments

--- api/health.go
+++ api/health.go
@@ -1,1 +1,11 @@
-package api
+package handler
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Health(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	fmt.Fprint(w, "ok")
+}
